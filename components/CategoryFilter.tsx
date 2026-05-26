a8b1c175fef656dac3859f7d98c0f8b9c9d2ae23
warnata2404 <411231103@mahasiswa.undira.ac.id>
2026-05-27 00:10:44 +0700

    feat: add category filter feature

---
+++ components/CategoryFilter.tsx
@@ -0,0 +1,166 @@
+import {
+  FlatList,
+  RefreshControl,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import { useEffect, useState } from "react";
+
+import COLORS from "../constants/colors";
+
+import CategoryFilter from "../components/CategoryFilter";
+import EmptyState from "../components/EmptyState";
+import ErrorState from "../components/ErrorState";
+import Loading from "../components/Loading";
+import ProductCard from "../components/ProductCard";
+
+import { getProducts } from "../services/productService";
+import { Product } from "../types/product";
+
+export default function HomeScreen() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [error, setError] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async (isRefreshing = false) => {
+    try {
+      if (isRefreshing) {
+        setRefreshing(true);
+      } else {
+        setLoading(true);
+      }
+
+      setError("");
+
+      const response = await getProducts();
+
+      setProducts(response);
+    } catch (err) {
+      setError("Failed to load products");
+    } finally {
+      setLoading(false);
+      setRefreshing(false);
+    }
+  };
+
+  const categories = [
+    "all",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.title
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+
+    const matchesCategory =
+      selectedCategory === "all" || product.category === selectedCategory;
+
+    return matchesSearch && matchesCategory;
+  });
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <ErrorState message={error} />;
+  }
+
+  if (filteredProducts.length === 0) {
+    return <EmptyState />;
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Product Catalog</Text>
+
+        <Text style={styles.totalText}>
+          {filteredProducts.length} Products Available
+        </Text>
+
+        <TextInput
+          placeholder="Search product..."
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          style={styles.searchInput}
+          placeholderTextColor={COLORS.gray}
+        />
+
+        <CategoryFilter
+          categories={categories}
+          selectedCategory={selectedCategory}
+          onSelectCategory={setSelectedCategory}
+        />
+      </View>
+
+      <FlatList
+        data={filteredProducts}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <ProductCard product={item} />}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => fetchProducts(true)}
+            colors={[COLORS.primary]}
+          />
+        }
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+
+  header: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: 16,
+  },
+
+  headerTitle: {
+    fontSize: 30,
+    fontWeight: "700",
+    color: COLORS.black,
+  },
+
+  totalText: {
+    fontSize: 14,
+    color: COLORS.gray,
+    marginTop: 4,
+    marginBottom: 16,
+  },
+
+  searchInput: {
+    backgroundColor: COLORS.white,
+    borderRadius: 14,
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+  },
+
+  listContent: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+});
